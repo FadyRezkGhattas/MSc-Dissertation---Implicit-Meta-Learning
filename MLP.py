16d13e159cf7d5e1174989f84e5138871010b629
FadyRezkGhattas <24905782+FadyRezkGhattas@users.noreply.github.com>
2021-08-01 17:19:52 +0200

    Add MLP model for experimentation

---
+++ MLP.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_dim=784, output_dim=10, nhid = 32):
+        super(MLP, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, nhid),
+            nn.LeakyReLU(),
+            nn.Linear(nhid, output_dim)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
