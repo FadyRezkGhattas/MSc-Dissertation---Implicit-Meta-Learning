7a7f90807c6db101072f8941e23a1bf2a888581f
FadyRezkGhattas <24905782+FadyRezkGhattas@users.noreply.github.com>
2021-08-08 19:07:43 +0200

    Add file to inspect confidence net output stats

---
+++ debug_hypernet.py
@@ -0,0 +1,79 @@
+import torch
+import argparse
+from Models.WideResnet import build_wideresnet
+import os
+from DatasetsUtil.DataLoaderWrap import DataLoaderWrap
+from torch.utils.data import  RandomSampler
+import csv
+
+def save_statistics(experiment_log_dir, filename, stats_dict, current_epoch, continue_from_mode = False, save_full_dict=False):
+    """
+    Saves the statistics in stats dict into a csv file. Using the keys as the header entries and the values as the
+    columns of a particular header entry
+    :param experiment_log_dir: the log folder dir filepath
+    :param filename: the name of the csv file
+    :param stats_dict: the stats dict containing the data to be saved
+    :param current_epoch: the number of epochs since commencement of the current training session (i.e. if the experiment continued from 100 and this is epoch 105, then pass relative distance of 5.)
+    :param save_full_dict: whether to save the full dict as is overriding any previous entries (might be useful if we want to overwrite a file)
+    :return: The filepath to the summary file
+    """
+    summary_filename = os.path.join(experiment_log_dir, filename)
+    mode = 'a' if continue_from_mode else 'w'
+    with open(summary_filename, mode) as f:
+        writer = csv.writer(f)
+        if not continue_from_mode:
+            writer.writerow(list(stats_dict.keys()))
+
+        if save_full_dict:
+            total_rows = len(list(stats_dict.values())[0])
+            for idx in range(total_rows):
+                row_to_add = [value[idx] for value in list(stats_dict.values())]
+                writer.writerow(row_to_add)
+        else:
+            row_to_add = [value[current_epoch] for value in list(stats_dict.values())]
+            writer.writerow(row_to_add)
+
+    return summary_filename
+
+def main(args):
+    meta_model = build_wideresnet(depth=28,
+                             widen_factor=2,
+                             dropout=0,
+                             num_classes=1)
+
+    saved_models = os.path.abspath(os.path.join(args.dir, "saved_models"))
+    checkpoint = torch.load(os.path.join(saved_models, args.checkpoint_name))
+
+    meta_model.load_state_dict(checkpoint['meta_model_dict'])
+
+    data_loader = DataLoaderWrap(args, RandomSampler, 0, 0)
+
+    targets = []
+    with torch.no_grad():
+        for i in range (5):
+            x, x_u, x_w = data_loader.get_unlabeled_batch()
+            targets += meta_model(x_u)
+            
+    return torch.mean(torch.stack(targets)), torch.std(torch.stack(targets))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch FixMatch Training')
+    parser.add_argument('--dir', default='', type=str, help='path to latest checkpoint (default: none)')
+    parser.add_argument('--checkpoint_name', default='', type=str, help='checkpoint to load')
+    parser.add_argument('--data', type=str, default='CIFAR10', choices=['CIFAR10', 'SVHN'], help='Name of Dataset (CIFAR10, MNIST or SVHN)')
+    parser.add_argument('--train_labelled_size', type=int, default=250, metavar='N', help='Number of training labelled samples from the dataset')
+    parser.add_argument('--validation_size', type=int, default=1000, metavar='N', help='Number of validation labelled samples from the dataset')
+    parser.add_argument('--labelled_batch_size', type=int, default=64, metavar='N', help='Batch size for each inner loop epoch')
+    parser.add_argument('--mu', type=int, default=7, metavar='N', help='Coefficient of unlabeled batch size')
+
+
+    args = parser.parse_args()
+    results = {'means':[],'stds':[]}
+
+    for i in range(100):
+        args.checkpoint_name= "epoch_85"+str(i)+".pth.tar"
+        mean, std = main(args)
+        results['means'].append(mean)
+        results['stds'].append(std)
+        
+    save_statistics(args.dir, "MWN_Outputs.csv", results, 0, False, True)
